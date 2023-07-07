4651eceb32209fb758afc4e78763bb827a63810e
Joele-E <esposito.joelebiz@gmail.com>
2023-07-07 12:18:32 +0200

    exercise done

--- exercise/es-23/exercise.js
+++ exercise/es-23/exercise.js
@@ -1,7 +1,14 @@
 const person = {
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+let keys = Object.keys(person);
+let values = Object.values(person);
+
+for (let i = 0; i < keys.length; i++) {
+  console.log(`${keys[i]}: ${values[i]}`);
 }
 
 // Print values of person using Object.keys
