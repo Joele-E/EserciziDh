b198f44a12122bc4ff65cf4b8919a95c13b19cdb
Joele-E <esposito.joelebiz@gmail.com>
2023-07-10 01:05:17 +0200

    exercise done

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,10 +1,42 @@
 class Person {
-  // ...
+  #firstName;
+  #lastName;
+  #age;
+  constructor(firstName, lastName, age) {
+    this.#firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  set firstName(myVal) {
+    this.#firstName = myVal;
+  }
+  get firstName() {
+    return this.#firstName;
+  }
+  set lastName(myVal) {
+    this.#lastName = myVal;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  set age(myVal) {
+    this.#age = myVal;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  get fullName() {
+    return `${this.#firstName} ${this.#lastName}`;
+  }
 }
 
-const person = new Person('Mario', 'Rossi', 25);
+const person = new Person("Mario", "Rossi", 25);
 console.log(person.fullName);
 
-person.firstName = 'Maria';
-person.lastName = 'Verdi';
+person.firstName = "Maria";
+person.lastName = "Verdi";
 console.log(person.fullName);
