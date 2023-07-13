b0b900f3acc26638cc2272de3076ce02a44e8cbc
Joele-E <esposito.joelebiz@gmail.com>
2023-07-13 11:45:13 +0200

    before changes

---
+++ exercise/exercise.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+const id = person.id;
+const firstName = person.firstName;
+const lastName = person.lastName;
+const age = person.age;
+
+console.log(id, firstName, lastName, age);
+g(AreaCalculator.calculate(circle));
