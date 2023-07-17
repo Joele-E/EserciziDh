9948a693f3a15df665856e573ad305d8229269d1
Joele-E <esposito.joelebiz@gmail.com>
2023-07-17 09:50:08 +0200

    exercise done

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,5 +1,5 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+function sum(...numbers) {
+  return numbers.reduce((number, added) => added + number);
 }
 
 console.log(sum(1, 2, 3, 4, 5));
