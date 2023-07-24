ab26281e1423f7a78a217fc0cfb1c2ee4c2d8aba
Joele-E <esposito.joelebiz@gmail.com>
2023-07-24 11:45:01 +0200

    exercise done

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,1 +1,27 @@
 const isLogged = true;
+
+function checkLogin(isLogged) {
+  return new Promise((resolve, reject) => {
+    let rand = Math.random();
+    if (isLogged) {
+      resolve(rand);
+    } else {
+      reject(new Error("Not logged in"));
+    }
+  });
+}
+
+function getInfo(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject(new Error("Could not find Info"));
+    }
+  });
+}
+
+checkLogin(isLogged)
+  .then(getInfo)
+  .then((info) => console.log(info))
+  .catch((err) => console.error(err));
